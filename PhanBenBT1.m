1c47df05204368584d3922a671b26ac1c1ed657f
Ben Phan <58044446+phanben110@users.noreply.github.com>
2021-11-21 11:07:31 +0700

    Ben update filename

--- PhanBenBT1.m
+++ PhanBenBT1.m
@@ -1,12 +1,11 @@
-% Bai Tap 1
-% Phan Ben - 18TDH1 - 105180273
-% SBD : 14
+% Bài Tập: 01
+% Số Thứ Tự Nhóm : 02
 a = [3 5 6 1];
-z_0 = 14;
+z_0 = 2;
 n = length(a);
 result = a(1);
 for j = 2:n
     result = result*z_0 + a(j);
 end
-disp('Result P3(14):')
+disp('Result P3(2):')
 x = result
